effac33712535b7819131c1f8e769b7a9fe75e9d
irrum6 <irrum6@github.com>
2021-12-15 15:54:51 +0400

    d153

--- src/help.rs
+++ src/help.rs
@@ -3,83 +3,8 @@ pub mod help {
         println!("help");
     }
     pub fn print_help2() {
-        let help_string = "# strgen
-        String Generator in rust 
-        
-        usage 
-        pass parameters (command line arguments) to get strings ($1, $2,...... $n ) 
-        $1 - first paramter -number of strings [required] 
-        $2 - string length [optional] defaults to 12 
-        $3 - mode [optional] defaults to random letter string 
-        $4 - next argument [optional] depends on mode 
-        $5 - write to file [strings.textout]  1 true , 0 or ommited false
-        
-        modes
-        
-        rls,10  RandomLetters,
-        rla,11  RandomLettersFromCustomAlphabet,
-        rlaf,12  RandomLettersFromAlphabetFile,
-        raw,21  RandomWord,
-        rawl,22  RandomWordFromListFile,
-        cow,31  CoupledWordsNouns,
-        cowe,32  CoupledWordsNames,
-        cowf,33  CoupledWordsListFiles,
-        
-        
-        rls|1x 
-        mode 
-        10 random leter string 
-        $4 -language [optional] defaults to english alphabet 
-        values 
-        ka georgian 
-        en english 
-        
-        mode 
-        rla,11 $4 alphabet 
-        
-        rlaf,12 randstr - $4 alphabet.file name 
-        
-        row 2x list string generator 
-        
-        mode 
-        $2 is ignored
-        21 random word  -> app-list
-        app will look for list in lists directory for following file name patters 
-        [listtype].[language].list 
-        types:nouns,adjectives,names 
-        
-        $4 -language 
-        values 
-        ka georgian 
-        en english 
-        de german 
-        
-        22 srandom word  -> list file 
-        $4 list file name 
-        example program 16 64 raw sample.list 
-        
-        sample.list 
-        benevolent,violent,active,passive,repressed,fearful,brave,heroic,punishable,desperate 
-        Benjamin,Brooklyn,Brooks,Bennett,Bella,Beau,Brayden,Bryson,Blake,Braxton 
-        
-        notice : $2 here refers to minimal length
-        
-        3x coupled words generator 
-        cow,31 coupled words -> app-list adjective_noun 
-        cowe,32 coupled words -> app-list adjective_name 
-        app will look for list in lists directory for following file name patters 
-        [listtype].[language].list 
-        types:nouns,adjectives,names 
-        
-        $4 -language values same as in raw,2x
-        
-        33 coupled words -> from list files 
-        $4 list file names  -> single string names separated with ':' 
-        example program 16 64 33 sample.list:sample2.list 
-        notice:: $2(length) does not have effect here 
-        
-        example: program 8 12 cow en 
-        example: program 8 12 cow en 1 if you want to write to file ";
+        use std::fs::read_to_string;
+        let help_string = read_to_string("HELP.md").expect("Something went wrong reading the file");
         println!("{}", help_string);
     }
 }
--- tests/one.rs
+++ tests/one.rs
@@ -1,15 +1,60 @@
 #[cfg(test)]
 pub mod tests {
-    // use strgen::gela;
+    // use stranameg::stringer::languages::languages::Languages;
+    use stranameg::stringer::*;
     #[test]
     fn unnamed() {
-        assert_eq!(1, 1);
-    }
-
-    #[test]
-    fn unnamed2() {
         use stranameg::stringer::print_help;
         print_help();
         assert_eq!(1, 1);
+    }
+    #[test]
+    pub fn check_len() {
+        let length = 12;
+        let len = String::from("12");
+        let ammount = String::from("16");
+        let mode = String::from("rls");
+        let lang = String::from("en");
+        let sarraya: [String; 5] = [String::new(), ammount, len, mode, lang];
+        let conf = Config::new(&sarraya);
+
+        let mut sg = LettterSequence::new("abc", length);
+        sg.setup(conf);
+        assert_eq!(sg.get().len(), length);
+    }
+    #[test]
+    pub fn is_georgian() {
+        //check if generated string is actually georgian alphabet
+        let len = String::from("12");
+        let ammount = String::from("16");
+        let mode = String::from("raw");
+        let lang = String::from("ka");
+        let sarraya: [String; 5] = [String::new(), ammount, len, mode, lang];
+        let conf = Config::new(&sarraya);
+
+        let lst = ListType::Nouns;
+        let lan = Languages::Georgian;
+        let mut sg = RandomWord::new(lst, lan);
+        sg.setup(conf);
+        let strong = sg.get();
+
+        let kachars = "აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ";
+        for c in strong.chars() {
+            let check = kachars.contains(c) || c == ' ';
+            assert_eq!(check, true);
+        }
+    }
+    #[test]
+    pub fn full_list() {
+        let ammount = String::from("16");
+        let sarraya: [String; 2] = [String::new(), ammount];
+        let conf = Config::new(&sarraya);
+        let english = Languages::English;
+        let nouns = ListType::Nouns;
+        let mut sg = RandomWord::new(nouns, english);
+        // stringer
+        sg.setup(conf);
+        //get_list_len
+        assert_ne!(sg.get_list_len(), 0);
     }
 }
